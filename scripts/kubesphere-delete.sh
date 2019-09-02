1e5a20fcaa05eddcc5292e569cf481a86a1c3f89
lilinlinlin <lilin@yunify.com>
2019-09-02 15:46:03 +0800

    删除pvc

--- scripts/kubesphere-delete.sh
+++ scripts/kubesphere-delete.sh
@@ -1,20 +1,5 @@
 #！/bin/bash
 
-# Copyright 2018 The KubeSphere Authors.
-#
-# Licensed under the Apache License, Version 2.0 (the "License");
-# you may not use this file except in compliance with the License.
-# You may obtain a copy of the License at
-# 
-#     http://www.apache.org/licenses/LICENSE-2.0
-# 
-# Unless required by applicable law or agreed to in writing, software
-# distributed under the License is distributed on an "AS IS" BASIS,
-# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-# See the License for the specific language governing permissions and
-# limitations under the License.
-
-
 #helm delete
 helms="elasticsearch-logging elasticsearch-logging-curator istio istio-init jaeger-operator ks-jenkins ks-sonarqube logging-fluentbit-operator metrics-server uc"
 
@@ -38,6 +23,18 @@ for namespace in `kubectl get namespaces|awk '{print $1}'|grep -v NAME`; do
         kubectl delete all --all -n $namespace
      else
         echo "namespace resource 已删除"
+     fi   
+done
+
+# pvc delete
+pvcs="kubesphere-system openpitrix-system kubesphere-monitoring-system kubesphere-devops-system kubesphere-logging-system"
+for pvcnamespace in `kubectl get pvc --all-namespaces|awk '{print $1}'|grep -v NAMESPACE`; do
+     result=$(echo $pvcs | grep "$pvcnamespace")
+     if [[ "$result" != "" ]]
+     then
+        kubectl delete pvc -n $pvcnamespace `kubectl get pvc -n kubesphere-system|awk '{print $1}'|grep -v NAME`
+     else
+        echo "pvc resource 已删除"
      fi   
 done
 
