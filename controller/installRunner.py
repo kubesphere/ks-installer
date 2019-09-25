e61824a8dfef6646819b86827cd57f117f4de511
pixiake <guotongxue123@163.com>
2019-09-25 19:06:59 +0800

    update controller

--- controller/installRunner.py
+++ controller/installRunner.py
@@ -6,7 +6,12 @@ import time
 import ansible_runner
 import os
 import yaml
+import collections
 
+
+# playbookBasePath = '/home/ubuntu/ks-installer/playbooks'
+# privateDataDir = '/etc/kubesphere'
+# configFile = '/home/ubuntu/ks-installer/playbooks/vars.yaml'
 
 playbookBasePath = '/kubesphere/playbooks'
 privateDataDir = '/etc/kubesphere'
@@ -28,15 +33,30 @@ ks_hook = '''
 }
 '''
 
+# Define components to install
+
 
 class component():
 
     def __init__(self, playbook, private_data_dir, artifact_dir, ident, quiet):
+        '''
+        :param private_data_dir: The directory containing all runner metadata needed to invoke the runner
+                                 module. Output artifacts will also be stored here for later consumption.
+        :param ident: The run identifier for this invocation of Runner. Will be used to create and name
+                      the artifact directory holding the results of the invocation.
+        :param playbook: The playbook that will be invoked by runner when executing Ansible.
+        :param artifact_dir: The path to the directory where artifacts should live, this defaults to 'artifacts' under the private data dir
+        :param quiet: Disable all output
+        '''
+
         self.playbook = playbook
         self.private_data_dir = private_data_dir
         self.artifact_dir = artifact_dir
         self.ident = ident
         self.quiet = quiet
+
+    # Generate ansible_runner objects based on parameters
+
 
     def installRunner(self):
         installer = ansible_runner.run_async(
@@ -48,8 +68,13 @@ class component():
         )
         return installer[1]
 
+# Operation result check
+
 
 def checkExecuteResult(interval=1):
+    '''
+    :param interval: Result inspection cycle. Unit: second(s)
+    '''
     taskProcessList = executeTask()
     completedTasks = []
     while True:
@@ -64,6 +89,8 @@ def checkExecuteResult(interval=1):
         time.sleep(interval)
         print("Please wait patiently for the tasks to complete !")
 
+# Execute and add the installation task process
+
 
 def executeTask():
     taskProcessList = []
@@ -77,6 +104,8 @@ def executeTask():
             taskProcess
         )
     return taskProcessList
+
+# Generate a objects list of components
 
 
 def generateTaskLists():
@@ -95,11 +124,12 @@ def generateTaskLists():
 
     return tasksDict
 
+# Generate a list of components to install based on the configuration file
+
 
 def getComponentLists():
     readyToEnabledList = []
     readyToDisableList = []
-    # current_path = os.path.abspath(os.path.dirname(__file__))
     global configFile
     if os.path.exists(configFile):
         with open(configFile, 'r') as f:
@@ -121,13 +151,41 @@ def getComponentLists():
     print(readyToDisableList)
     return readyToEnabledList, readyToDisableList
 
+def preInstallTasks():
+    preInstallTasks = collections.OrderedDict()
+    preInstallTasks['preInstall'] = [
+            os.path.join(playbookBasePath, 'preinstall.yaml'),
+            os.path.join(privateDataDir, 'preinstall')
+        ]
+    preInstallTasks['plugins'] = [
+            os.path.join(playbookBasePath, 'plugins.yaml'),
+            os.path.join(privateDataDir, 'plugins')
+        ]
+    preInstallTasks['common'] = [
+            os.path.join(playbookBasePath, 'common.yaml'),
+            os.path.join(privateDataDir, 'common')
+        ]
+
+    # print(preInstallTasks)
+    # print(preInstallTasks['preInstall'])
+    for task, paths in preInstallTasks.items():
+        ansible_runner.run(
+            playbook=paths[0],
+            private_data_dir=privateDataDir,
+            artifact_dir=paths[1],
+            ident=str(task),
+            quiet=False
+        )
+
 
 def main():
     if len(sys.argv) > 1 and sys.argv[1] == "--config":
         print(ks_hook)
     else:
-        time.sleep(10)
-        checkExecuteResult()
+        # time.sleep(10)
+        # execute preInstall tasks
+        preInstallTasks()
+        # checkExecuteResult()
         print("successful")
 
 
