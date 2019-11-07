c64623d5adc9b31f0d567d34a2de166a1e36ae09
pixiake <guotongxue123@163.com>
2019-11-07 21:25:52 +0800

    add pip ignore installed

--- src/scripts/os/ubuntu.sh
+++ src/scripts/os/ubuntu.sh
@@ -39,9 +39,8 @@ sudo apt-get install python-minimal libssl-dev sshpass -y
 
 sudo python os/get-pip.py
 
-pip install -U PyYAML
+pip install -U --ignore-installed pyopenssl PyYAML
 
-pip install  -r os/requirements.txt
+pip install --ignore-installed -r os/requirements.txt
 
-pip install pyopenssl
 
