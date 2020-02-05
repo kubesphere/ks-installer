1385c03ec063ba2520a7fb09e2080dec177a65df
周鹏飞@yunify <pengfeizhou@yunify.com>
2020-02-05 16:42:31 +0800

    update installation instruction
    Signed-off-by: 周鹏飞@yunify <pengfeizhou@yunify.com>

--- src/scripts/install.sh
+++ src/scripts/install.sh
@@ -79,7 +79,7 @@ $(echo -e "\033[1;36mPrerequisites:\033[0m")
 
 1. It's recommended that Your OS is clean (without any other software installed), otherwise there may be conflicts.
 
-2. OS requirements (For Minimal Installation only)：2 Core or faster processor, 4GB or more of RAM.
+2. OS requirements (For Minimal Installation only)：at least 2 vCPUs and 4GB RAM.
 
 3. Please make sure the storage service is available if you've configured storage parameters in the conf directory .
 
