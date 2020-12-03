a9aa9de846f13b29d3c57cb8ddbccf9c40320ee6
shaowenchen <mail@chenshaowen.com>
2020-12-03 11:45:14 +0800

    fix: modify check script
    Signed-off-by: shaowenchen <mail@chenshaowen.com>

--- .github/workflows/deploy/install.sh
+++ .github/workflows/deploy/install.sh
@@ -4,14 +4,17 @@ yum install -y vim openssl socat conntrack ipset
 echo -e 'yes\n' | /root/kk create cluster --with-kubesphere
 
 
-kubectl -n kubesphere-system patch cc ks-installer --type merge --patch '{"spec":{"devops":{"enabled":"true"}}}'
-kubectl -n kubesphere-system patch cc ks-installer --type merge --patch '{"spec":{"logging":{"enabled":"true"}}}'
-kubectl -n kubesphere-system patch cc ks-installer --type merge --patch '{"spec":{"metrics_server":{"enabled":"true"}}}'
+kubectl -n kubesphere-system patch cc ks-installer --type merge --patch '{"spec":{"devops":{"enabled":true}}}'
+kubectl -n kubesphere-system patch cc ks-installer --type merge --patch '{"spec":{"logging":{"enabled":true}}}'
+kubectl -n kubesphere-system patch cc ks-installer --type merge --patch '{"spec":{"metrics_server":{"enabled":true}}}'
 
-for ((n=0;n<6;n++))
+kubectl -n kubesphere-system rollout restart deploy ks-installer
+
+for ((n=0;n<12;n++))
 do  
     OK=`kubectl get pod -A| grep -E 'Running|Completed' | wc | awk '{print $1}'`
-    Status=`kubectl get pod -A | wc | awk '{print $1}'`
+    Status=`kubectl get pod -A | sed '1d' | wc | awk '{print $1}'`
+    echo "Success rate: ${OK}/${Status}"
     if [[ $OK == $Status ]]
     then
         n=$((n+1))
