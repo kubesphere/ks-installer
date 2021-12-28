be7efba891194f0810d0e7a35ad7138cff767fe9
pixiake <guofeng@yunify.com>
2021-12-28 10:46:22 +0800

    update version to v3.2.1-rc.0
    Signed-off-by: pixiake <guofeng@yunify.com>

--- controller/installRunner.py
+++ controller/installRunner.py
@@ -46,7 +46,7 @@ cluster_configuration = {
         "name": "ks-installer",
         "namespace": "kubesphere-system",
         "labels": {
-            "version": "qkcp"
+            "version": "v3.2.1-rc.0"
         },
     },
 }
