ba7946fc860dcd5ca9d41a277dadb8e901ce3c41
mabing <bing.ma@daocloud.io>
2022-07-02 20:06:51 +0800

    remove \r when push to local registry

--- scripts/offline-installation-tool.sh
+++ scripts/offline-installation-tool.sh
@@ -198,6 +198,8 @@ elif [ -n "${ImagesList}" ]; then
           fi
 
           ## push image
+          image=$(echo "${image}" |tr -d '\r')
+          imageurl=$(echo "${imageurl}" |tr -d '\r')
           echo $imageurl
           docker tag $image $imageurl
           docker push $imageurl
